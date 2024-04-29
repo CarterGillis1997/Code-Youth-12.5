ce571523c1c534b5e73b63021ce9f221639c1849
CarterGillis1997 <92799737+CarterGillis1997@users.noreply.github.com>
2024-04-29 14:07:58 -0400

    MORE

--- if.js
+++ if.js
@@ -1,3 +1,4 @@
+let timer;
 console.log(50 == 50)
 console.log(50 == 60)
 console.log(50 == "50")
@@ -66,3 +67,76 @@ for(let i = 0; i <= 10; i++){
 for(let i = 0; i <= 50; i+=10){
     console.log(i)
 }
+
+function loopFunction(){
+    for(let i = 0; i < 5; i++){
+        console.log("I am in the function")
+    }
+}
+
+loopFunction()
+loopFunction()
+
+function increase(num){
+    num++
+    return num;
+}
+function decrease(num){
+    num--;
+    return num;
+}
+
+let number1 = 0;
+
+function increaseOrDecrease(mode){
+    if(mode == "increase"){
+        number1 = increase(number1);
+    }else if(mode == "decrease"){
+        number1 = decrease(number1);
+    }
+    console.log(number1)
+}
+
+increaseOrDecrease("increase")
+increaseOrDecrease("increase")
+increaseOrDecrease("increase")
+increaseOrDecrease("increase")
+
+increaseOrDecrease("decrease")
+
+for(let i = 0; i < 10; i++){
+    increaseOrDecrease("increase")
+}
+
+function howdyFunction(){
+    console.log("howdy")
+}
+
+timer = setTimeout(howdyFunction, 5000);
+
+// clearTimeout(timer)
+
+let outerVariable = "five"
+
+function a(){
+    console.log(outerVariable)
+    let innerVariable = "ten";
+    console.log(innerVariable)
+
+}
+
+function b(){
+    let float = 4.5
+    for(let i = 0; i < 10; i++){
+        console.log(float)
+    }
+}
+
+a()
+b();
+
+function c(){
+    console.log("C IS RUNNING !!!!!")
+}
+
+c()
