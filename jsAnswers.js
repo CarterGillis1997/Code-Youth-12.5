592c3b7a6b632e76dfb30eabe39c17f60d7833fb
CarterGillis1997 <92799737+CarterGillis1997@users.noreply.github.com>
2024-04-29 14:39:03 -0400

    aaaaa

---
+++ jsAnswers.js
@@ -0,0 +1,14 @@
+// Create a variable called name and give it the value of your name
+
+// Create two variables that have number values. console.log the sum of them.
+
+// Create a variable that has the value "hello". Try adding another string to it and console.log the result
+
+// Write a function that will take two numbers and return the sum
+
+// Write an if statement that will check to see if numVariable is less than 10, greater than 10, or equal to ten. Make sure to use console.log to show on the console
+let numVariable = 42;
+
+// Write a for loop that counts from 0 to 10. In the console it should look like this 012345678910
+
+// Write an if statement that checks if the first letter in a string is a vowel.
