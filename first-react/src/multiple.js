977b9caed6c76b1174974d93a45e0a97c5e20883
CarterGillis1997 <92799737+CarterGillis1997@users.noreply.github.com>
2024-05-07 13:20:38 -0400

    update

--- first-react/src/multiple.js
+++ first-react/src/multiple.js
@@ -1,41 +1,50 @@
 import React from "react";
 
-class Multiple extends React.Component{
-    constructor(props){
+class Multiple extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-            fruits:["Apple","Mango","Dragonfruit","Kiwi","Banana","ORANGE"],
-            input:"",
-            options:["backgroundColor","color","border","width","height"]
+            fruits: ["Apple", "Mango", "Dragonfruit", "Kiwi", "Banana", "ORANGE"],
+            input: "",
+            options: ["backgroundColor", "color", "border", "width", "height"]
         }
     }
+
+    componentDidMount = () => {
+        console.log("Hello")
+    }
+
+    componentDidUpdate = () => {
+        console.log("UPDATE")
+    }
+
+
     handleInput = (event) => {
         this.setState({
-            input:event.target.value
+            input: event.target.value
         })
     }
 
     handleClick = () => {
-        this.setState(state=>({
-            fruits:[...state.fruits, state.input]
+        this.setState(state => ({
+            fruits: [...state.fruits, state.input]
         }))
     }
 
-    render(){
-        let elements = this.state.fruits.map((element, i)=>{
-            return(
+    render() {
+        let elements = this.state.fruits.map((element, i) => {
+            return (
                 <li key={i}>{element}</li>
             )
         })
 
-        let optionElements = this.state.options.map((element, i)=>{
-            return(
+        let optionElements = this.state.options.map((element, i) => {
+            return (
                 <option key={i}>{element}</option>
             )
         })
 
-        console.log(elements)
-        return(
+        return (
             <div>
                 <h2>Fruit</h2>
                 <ul>
